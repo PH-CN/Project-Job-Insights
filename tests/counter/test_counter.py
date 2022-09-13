cffac2d0fee844176ebe4a6e1ea1db5c889e80d8
Pedro Candido <pedrohcn02@gmail.com>
2022-09-13 14:56:50 -0300

    requisito 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,9 @@
-# from src.counter import count_ocurrences
+from src.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    path = "src/jobs.csv"
+    words = ["the", "man"]
+    expected = [50865, 9799]
+    for index in range(len(words)):
+        assert count_ocurrences(path, words[index]) == expected[index]
